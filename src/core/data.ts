8470cf38e050c80a3ff60382835e271fa146071a
Dmitry Shevchenko <dmitry.shevchenko@playtech.com>
2017-11-15 03:02:26 +0200

    add references to current page in paginator href attributes

--- src/core/data.ts
+++ src/core/data.ts
@@ -3,7 +3,10 @@ import * as v from "./utils";
 export default function (this: uibPaginationData) {
 
     let currentPage = v.isDefined(this.value.currentPage) ? this.value.currentPage : 1;
-    let data = { currentPage };
+    let data = {
+        currentPage,
+        origin: location.origin
+    };
 
     return data;
 };
